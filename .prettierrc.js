e1bf24e9d3e2686b3e9b67e60e57ec9116b94285
Swastik Agarwal <swastik@customer.io>
2020-11-25 18:31:11 +0530

    Remove bower references

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: ["*.js", ".*.js", "*.ts", ".*.ts"],
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
+};
